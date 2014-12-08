a6a5280f9a03d1cb5f5aa633544f92ba38e2104f
jcseo3 <jseo@colgate.edu>
2014-12-08 16:38:03 -0500

    function for fixing FAT chain when it's longer and shorter; not quite done with the shorter func, but had some problems with that; comments have some of my questions so if you have answers, awesome. If not, we can talk!

--- scandisk.c
+++ scandisk.c
@@ -1,3 +1,8 @@
+/*
+Names: Junghyun Seo , Cindy Han
+Proj05
+*/
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -20,6 +25,52 @@ void usage(char *progname) {
     exit(1);
 }
 
+//gets the current actual length of the cluster chain so that it can be compared to the size value in the directory entry
+int getclusterlen(uint16_t startCluster, uint8_t *image_buf, struct bpb33 *bpb){
+	int clusterlen = 1;
+	uint16_t fatent = get_fat_entry(startCluster, image_buf, bpb);
+
+	while (!is_end_of_file(fatent)){
+		clusterlen++;
+		fatent = get_fat_entry(startCluster, image_buf, bpb);
+	}
+	return clusterlen;
+}
+
+//Fix for when the FAT cluster chain is LONGER than the given correct size in the dir entry 
+void fix_lFAT(uint16_t startCluster, uint8_t *image_buf, struct bpb33 *bpb, uint32_t clusterlen){
+	//can we always assume there will be at least one cluster? for now, I assumed that we also assume there is always one
+	int clusternum = 1;
+	uint16_t fatent = get_fat_entry(startCluster, image_buf, bpb);
+	
+	while (clusternum < clusterlen){
+		fatent = get_fat_entry(startCluster, image_buf, bpb);
+		clusternum++;
+	}
+		
+	//set the "real" last cluster to EOF
+	set_fat_entry (fatent, EOF, image_buf, bpb);
+	
+	//freeing any clusters that exist past the real last cluster
+	while (!is_end_of_file(fatent)){
+		uint16_t freetemp = fatent;
+		fatent = get_fat_entry(startCluster, image_buf, bpb);
+		set_fat_entry (freetemp, EOF, image_buf, bpb); // I'm don't think setting it to EOF is freeing it, but Idk how to free it.. 		
+	}	
+}
+
+//Fix for when the FAT chain cluster is SHORTER than the given correct size in dir entry
+void fix_sFAT(uint16_t startCluster, uint8_t *image_buf, struct bpb33 *bpb, struct direntry *dirent){
+	//get the size that is recorded in the directory entry
+	uint32_t size = getulong(dirent->deFileSize);
+	int correctclusternum = size / 512
+	if ((size % 512) != 0){
+		correctclusternum+=1;
+	} 
+	//traverse the clusters and undo the EOF on the current EOF and keep traversing until end of numofclusters and make that last FAT EOF 
+	//problem: do I overwrite other clusters or do I jump around? how does this work...? how do I lengthen the chain cluster without overwriting others?
+}
+
 
 int main(int argc, char** argv) {
     uint8_t *image_buf;
@@ -29,14 +80,12 @@ int main(int argc, char** argv) {
 	usage(argv[0]);
     }
 
-    image_buf = mmap_file(argv[1], &fd);
-    bpb = check_bootsector(image_buf);
+    image_buf = mmap_file(argv[1], &fd); //image_buf is a pointer to the memory-mapped disk image;can pass pointer along other funcs to read and manipulate FS
+    bpb = check_bootsector(image_buf); // checks to makes sure bootsector is valid
+    
+    uint8_t rootdiraddr = *root_dir_addr(image_buf, bpb);
 
-    // your code should start here...
-
-
-
-
+	//your code should start here...
 
 
     unmmap_file(image_buf, &fd);
